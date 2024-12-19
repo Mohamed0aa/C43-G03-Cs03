f2470c8009b0debee661c992a9e82e7d7599d412
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2024-12-19 05:53:43 +0200

    q15

--- assignmet/Program.cs
+++ assignmet/Program.cs
@@ -137,6 +137,18 @@ namespace assignmet
             //Console.WriteLine(13 / 2 + " " + 13 % 2);
             ////6 1
             #endregion
+
+            #region q15 : What will be the output of the C# code given below?
+            //int num = 1, z = 5;
+
+
+            //if (!(num <= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z);
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+
+            ////7 7
+            #endregion
         }
     }
 }
