5f5b0d3abf6fad35cdf042337955d0722b0fb08e
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2024-12-19 03:36:35 +0200

    q2

--- assignmet/Program.cs
+++ assignmet/Program.cs
@@ -11,6 +11,14 @@ namespace assignmet
             //if(n%3==0 && n%4==0) Console.WriteLine("yes");
             //else Console.WriteLine("no");
             #endregion
+
+            #region q2 : Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+            //int n = int.Parse(Console.ReadLine());
+            //if (n < 0) Console.WriteLine("negative");
+            //else Console.WriteLine("positive");
+            #endregion
+
+            
         }
     }
 }
