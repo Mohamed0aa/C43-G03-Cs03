0bd65cc44b19242ecb2db64cea0d328ffdb4e2e6
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2024-12-19 03:31:34 +0200

    q1

---
+++ assignmet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Channels;
+
+namespace assignmet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region q1 : Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+            //int n=int.Parse(Console.ReadLine());
+            //if(n%3==0 && n%4==0) Console.WriteLine("yes");
+            //else Console.WriteLine("no");
+            #endregion
+        }
+    }
+}
